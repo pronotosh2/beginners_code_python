c59b2e7d357bf9279f34b7f36060f1232986c22a
pronotosh2 <127610730+pronotosh2@users.noreply.github.com>
2023-03-17 10:45:45 +0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,45 @@
+print("Hello World")
+
+print("Mosh Hamedani")
+print("*" * 10)
+prize=10
+prize=20
+print(prize)
+
+Full_name = "John Smith"
+Age = 20
+is_new = True
+name =input("What is your name?")
+print("Hi" + name)
+favorite_color = input("What is your favorite color?")
+print(name + " likes " + favorite_color)
+
+birth_year = input(" Birth year:")
+age = 2022 - int(birth_year)
+print(age)
+weight_lbs = input("Weight (lbs): ")
+weight_kg = int(weight_lbs) * 0.45
+print(weight_kg)
+course = "Python's course for Beginners"
+print(course)
+course = ' Python for "Beginners"'
+print(course)
+
+course = '''
+Hi John,
+Here is our first email to you.
+
+Thank you,
+The support team 
+
+'''
+print(course)
+
+course = 'Python for Beginners'
+print(course[0])
+print(course[1])
+print(course[2])
+print()
+print(course[-1])
+print(course[-2])
+print(course[-3])
