c4e0573d4b7ec1fe0c33e7801b8b0040e922f60a
pronotosh2 <127610730+pronotosh2@users.noreply.github.com>
2023-03-17 15:47:56 +0600

    Create Class01-2:00pm-07-03-2023-City_IT_Park-Khalishpur.py

---
+++ Class01-2:00pm-07-03-2023-City_IT_Park-Khalishpur.py
@@ -0,0 +1,159 @@
+# /*
+# Introduction to Python Programming 
+# Our Instructor's Name : Akash
+
+# Python is a high-level, general-purpose programming language.
+# Its design philosophy emphasizes code readability with the use of significant indentation.
+
+#                                   Flask is used for developing web applications using python.
+
+#                                   Django is a high-level Python web framework that encourages rapid development and clean, pragmatic design.
+#                                   Django is an open-source framework for backend web applications based on Python.
+
+#                                   Flask provides support for API, while Django doesn't have any support for API.
+
+# Python is 2nd popular language. It is used for Data Science,AI,Machine Learning, Image Processing etceteras.
+# --easy to learn 
+# --competitive programming
+# --compiler 
+# --very slow 
+# --some has no option for python 
+
+
+# Now,
+# Open your browser and write url: https://www.python.org/
+#     or search in google writing python and you can find it just in second.  During installation try to follow Custom Install and check all option in Optional Features for future use.
+    
+
+    
+# And now we need an IDE or Code editor for writing the code.
+#                      Popular IDE softwares are heavy softwares and need more time.
+#               Code editor:
+#               ---where code is written 
+#               ---manual environment 
+#               ---need 2/3 extension 
+
+# For this course we will use a popular and free software : VS Code also known as Microsoft Visual Studio Code
+  
+  
+# After opening your browser write url: https://code.visualstudio.com/
+#      or search in google writing VS Code and you can find it just in second. During installation select all add tasks for future use. 
+# We will install two extension in VS Code:
+# 1-- Python           from microsoft.com
+# 2-- vscode-icons     from vscode iconcodeicons.team
+
+
+# Visual Studio Code is a code editor.
+# A developer can easily edit their code. VS is slower when it comes to performing across different platforms.
+
+
+# Please skip 16 lines for a quick introduction.
+   
+  
+  
+  
+  
+#        For More:
+#        Visual Studio vs Visual Studio Code
+#        Visual Studio aids in developing computer programs, websites, web applications, mobile applications, and web services. Visual Studio or IDE takes the help of Microsoft’s software development platform, i.e., Windows API, Windows Presentation Foundation, Windows Forms, Microsoft Silverlight, and Windows Store, to produce and manage native code.
+
+#        Whereas Visual Studio Code can be used to write, edit, and debug the code, all in one place. VS Code supports many programming languages, for which a developer does not require Web Support. Everything can be found in its built-in multi-language support.
+
+#        A developer can rely on Visual Studio Code for all kinds of development, as long as they pair it with the right tools. VS Code comes with built-in support for JavaScript, Node.js, and TypeScript. If this is not enough, one can easily add the necessary support for languages like Python, C#, PHP, Java, and many more by installing their extensions.
+
+#        The primary reason why VS Code seems better in terms of usage in this debate of Visual Studio vs Visual Studio Code is that it offers incredible support, it can work across all platforms, and is lightweight and robust. It can do everything in just one place.
+
+
+
+# Now are entering into basic.
+
+# ()        parenthesis
+# {}        curly braces 
+# []        Square Bracket
+# <>        angle bracket
+# ''        single Quotation marks
+# ""        double Quotation marks
+# /         Slash          or forward slash
+# \         backslash             is used only for computer coding. 
+# `         Back comma
+# ``        Back comma Quotation
+
+# Above are the basics.
+# ''''''   String literals inside triple quotes, """ or ''', can span multiple lines of text. Python strings are "immutable" which means they cannot be changed after they are created (Java strings also use this immutable style).
+
+
+# Data types of python:                                              For details: https://www.w3schools.com/python/python_datatypes.asp 
+# integer (int)                                                     a whole number (not a fractional number) that can be positive, negative, or zero.
+# string  (str)                                                     string is a sequence of characters.
+# float   (float)                                                   Float is a function or reusable code in the Python programming language that converts values into floating point numbers. Floating point numbers are decimal values or fractional numbers like 133.5 
+# boolean (bool)                                                    It's used to represent the truth value of an expression. In computing, the term Boolean means a result that can only have one of two possible values: true or false.
+
+# Open VS Code and We will see a wecome note.There you can see New file click and name it like Class01.py or any thing you want. Press enter and it will open a window named Create File. Just press enter.
+# Now you are ready to write your First code.
+# */
+
+print()                   # it is our first as its parenthesis has contained nothing after running it will space(nothing) . If possible skip this line.
+
+print(2)                  # Run or Ctrl+F5   and   TERMINAL: 2
+
+print(10)                 #TERMINAL: 10 
+
+print("Akash")            #TERMINAL: Akash 
+
+print(2+2)                #4
+
+print('2'+"2")            #22 
+
+print('2'-"2")            #TypeError: unsupported operand type(s) for -: 'str' and 'str'
+
+print(2+"2")              #TypeError
+
+
+
+                           # character in python is written as str 
+print("akash"+str(2))      # akash2
+
+print(20.2+2)              #22.2 
+
+print(2)                    #2
+
+print(type(2))              #<class 'int'>
+
+print(type('2'))            #<class 'str'>
+
+print(type(True))           #<class 'bool'> 
+
+
+
+        # We need to open two account
+        #github       and website: https://github.com/                and sign up there 
+        #           for github you need a email, password and username. After that you have a update notification type y or n,
+        #           Then verify your account  by solving two puzzle and click create account.
+        #           Now you will receive a code in email(email that you entered) from GitHub. 
+        #           After entering the code you will see a new page Initializing your dashboard.
+        #           Congrats   you have a GitHub account now. 
+        #
+        # 
+        #  
+        #stackoverflow      and website:  https://stackoverflow.com/         and sign up there
+        #                Now you need Display , Email , Password and click I'm not a robot .     Then Sign Up 
+        #                Registration email will be sent to your email . Now open that email. In email click Complete your registration and you will be redirect to your account. 
+        # 
+        #For help or any doubt in this class01 ----01990 709752 
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
