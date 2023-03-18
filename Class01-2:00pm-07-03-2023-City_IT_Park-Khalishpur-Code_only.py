cc74b52e0be98efb0ee44e0ca493d8d3d20761eb
pronotosh2 <127610730+pronotosh2@users.noreply.github.com>
2023-03-18 19:43:38 +0600

    Create Class01-2:00pm-07-03-2023-City_IT_Park-Khalishpur-Code_only.py

---
+++ Class01-2:00pm-07-03-2023-City_IT_Park-Khalishpur-Code_only.py
@@ -0,0 +1,30 @@
+print()                      # it is our first as its parenthesis has contained nothing after running it will space(nothing) . If possible skip this line.
+
+print(2)                     # Run or Ctrl+F5   and   TERMINAL: 2
+
+print(10)                    #TERMINAL: 10 
+
+print("Akash")               #TERMINAL: Akash 
+
+print(2+2)                   #4
+
+print('2'+"2")               #22 
+
+print(2-2)                   #0                             before              TypeError: unsupported operand type(s) for -: 'str' and 'str'
+
+print(2+2)                   #4                             before              TypeError
+
+
+
+                             # character in python is written as str 
+print("akash"+str(2))        # akash2
+
+print(20.2+2)                #22.2 
+
+print(2)                     #2
+
+print(type(2))               #<class 'int'>
+
+print(type('2'))             #<class 'str'>
+
+print(type(True))            #<class 'bool'> 
