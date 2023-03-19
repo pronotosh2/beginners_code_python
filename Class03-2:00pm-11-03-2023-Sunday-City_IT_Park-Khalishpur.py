603660c9836e9ea4bac3fac247e31ac8e25c1151
pronotosh2 <127610730+pronotosh2@users.noreply.github.com>
2023-03-20 01:34:32 +0600

    Create Class03-2:00pm-11-03-2023-Sunday-City_IT_Park-Khalishpur.py

---
+++ Class03-2:00pm-11-03-2023-Sunday-City_IT_Park-Khalishpur.py
@@ -0,0 +1,90 @@
+                            #Class03-2:00pm-11-03-2023-Sunday-City_IT_Park-Khalishpur       Temperature 21 degree
+                 #github.com/milonkumar11
+# Discussion about polymorphism in python, type casting--> type conversion
+# polymorphism : doing same work in different ways ( multiple ways )
+
+#array ---> all programming languages have array but python has list (one kind of array)
+
+# casting ---> conversion from one data type to another data type(eg. str to int) or conversion
+
+
+a=20                            # int data type                   ctrl+S
+print(str(a))                   #20                                  # Run command F5
+print(type(str(a)))             #<class 'str'>
+print(type(a))                  #<class 'int'>
+
+
+a="20"                          # str data type
+print(type(int(a)))             #<class 'int'>
+
+              #List/Array:  if same data type data in plural numbers are stored in a variable that is called List or array.
+  
+a=["a","b","c","d",'e','f']
+print(a)                        #['a', 'b', 'c', 'd', 'e', 'f']
+print(a[2])                     #c
+                        
+                        #adding new data in list             a.append    from appearence in end
+
+a=["a","b","c","d",'e','f']
+a.append("talgas")
+print(a)                                     #['a', 'b', 'c', 'd', 'e', 'f', 'talgas']
+
+                     #Removing from list 
+
+a=["a","b","c","d",'e','f']
+a.append("biral")
+a.remove("c")
+print(a)                                     #['a', 'b', 'd', 'e', 'f', 'biral']
+
+                        #again we will declare a variable 'a' and asign the operator '=' and then will make a list
+
+a=["rimpa","sima"]
+a.append("akash")
+a.remove("rimpa")
+print(a)                                    #['sima', 'akash']
+
+                    #If we need to add something in any list then the keyword is    variable.insert(index number,item)
+
+a=["a","b","c","d",'e','f']
+a.insert(3,300)
+print(a)                                    #['a', 'b', 'c', 300, 'd', 'e', 'f']
+
+a=["a","b","c","d",'e','f']
+a.insert(0,300)
+print(a)                                    #[300, 'a', 'b', 'c', 'd', 'e', 'f']
+   
+                   #if anyone want to practice in mobile
+                   #w3schools.com > Python > Learn Python > Start learning python now > Try yourself  ( right side)
+                
+
+                #operator: Operators are arithmetic, logical, and relational.some kinds of sign used to do logical works. 
+                # and 
+                # operand: something (such as a quantity or data) that is operated on (as in a mathematical operation)
+
+                # a+b  here a , b are operands and     + is operator 
+
+a=10
+n=a=a+1
+print(n)                                    #11
+
+a=10
+n=a+1
+print(n)                                    #11
+
+a=10
+a=a+1
+print(n)                                    #11
+
+a=10
+a+=1
+print(n)                                    #11
+
+
+                          # "=" is assign operator 
+                          # "==" is equal , comparison operator 
+                                                                                                                                                       #ends in 4:29 pm
+
+                       #For any question about this class , Our instructor:01990 709752 
+
+
+                        #        Thanks for your patience.
