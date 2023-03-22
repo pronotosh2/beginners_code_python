4e82c18239c3f50110c614e92a022db982b4bf24
pronotosh2 <127610730+pronotosh2@users.noreply.github.com>
2023-03-22 23:27:37 +0600

    Update Class02-2:00pm-09-03-2023-Thursday-City_IT_Park-Khalishpur.py

--- Class02-2:00pm-09-03-2023-Thursday-City_IT_Park-Khalishpur.py
+++ Class02-2:00pm-09-03-2023-Thursday-City_IT_Park-Khalishpur.py
@@ -1,6 +1,6 @@
                                                                        #   Class02-2:00pm-09-03-2023-Thursday-City_IT_Park
                 # variable declaration in python
-                                                                # OOP in not included 
+                                                                # OOP is not included 
 
 
 shemul = 20
@@ -14,7 +14,7 @@ akash="Monirul Islam Akash"
 akash[5]
 print(akash[5])                                        # u
 
-                                                            # Python is a case sensitive language. So Akash is not akash
+                                                                # Python is a case sensitive language. So Akash is not akash
 len(akash)                                          # keyword for length 
 print(len(akash))                                   # 19 
 
@@ -41,8 +41,11 @@ print(akash[9:])                                    #ong established fact that a
                                                                                                                                                                                                                              # step : It is an optional argument that determines the increment between each index for slicing.
 print(akash[0:0])                                   #
   
+
                     #printing slicing  0 to 1, not including 1
 print(akash[0:1])                                   #I
+
+
 
                     #printing slicing  0 to 2, not including 2
 print(akash[0:2])                                   #It
@@ -50,7 +53,13 @@ print(akash[0:3])                                   #It
 print(akash[0:4])                                   #It i
 print(akash[0:5])                                   #It is
 
+
+
 print()                                              #
+
+
+
+
 
                   #printing slicing  -5 to infinity, not including infinity               
 print(akash[-5:])                                      #ike).
@@ -60,9 +69,14 @@ print(akash[-5:-3])                                  #ik
 print(akash[-5:-4])                                  #i
 print(akash[-5:-5])                                  #
 
-print(akash[10:15])                                   #ng es
-print()                                               #
-print(akash[10:len(akash)])                            #ng established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+
+
+print(akash[10:15])                                    #ng es
+
+print()                                                #
+
+print(akash[10:len(akash)])                             #ng established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+
 print()       
 
 print(akash[10:])                                       #ng established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
@@ -72,26 +86,48 @@ print()
 print()       
 print()       
  
-# printing after slicing -613 to -12 , not including -12           in other sense printing after excluding last 12 letters
+
+
+
+# printing after slicing -613 to -12 , not including -12            in other sense, printing after excluding last 12 letters
 
 print(akash[-613:-12])                                  # #ng established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour an
+
+
+
+
+
+
 
 
 akash="ABCDEFGHIJ"
 print(akash[3:])   
 
+
+
 a=input("input: ")                                      #TERMINAL: input: akash
 print("output: ",a)                                    #  akash
+
+
+
 
 a=input("input: ")                                     # input: akash 
 print("output:"+a)                                     #akash              there are space difference in this two 
 
+
+
+
 a=input("input a: ")                                   #input a: 20
 b=input("input b: ")                                   #input b: 20
 print(a+b)                                             #2020
-                                                       #generally input is taken as str
+                                                       #generally input is taken as data type str
 
-                                 #type conversion
+
+
+
+
+
+                                 # topic:type conversion
                                      #int() Converts to an integer.
                                                                                 #float() Converts to a float.
                                                                                 #str() Converts to a string.
@@ -101,18 +137,24 @@ print(a+b)                                              #40
 print(type(a))                                          #<class 'int'>
 
 
+
 print(type(True))                                       #<class 'bool'>
+
 
 a="habibullah"
 print(a[7:])                                            #lah
 print(a[-3:])                                           #lah
 
                                                                       #we are seeing variable declaration
-                                            
+
+
+
 a=int(input())                                          #10
 b=int(input())                                          #10
 print(a+b)                                              #20
 print(type(a+b))                                        #<class 'int'>
+
+
 
 
 mim="Symaiya Akter Mim"
@@ -126,7 +168,18 @@ print(mim[3:-3])                                        #aiya Akter
  
   
     
-    
-      # one Extra (little complicated)
+
+
+      # one Extra (little complicated) skip if possible 
+
 print(akash[::-1])                                      #JIHGFEDCBA
 print(akash[-5::-1])                                    #FEDCBA
+
+
+
+
+
+                       #For any question about this class , Our instructor:01990 709752 
+
+
+                        #        Thanks for your patience.
