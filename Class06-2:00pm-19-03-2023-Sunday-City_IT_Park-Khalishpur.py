c3b70108f3cec68c8730c5b728c6cbdd21014dcd
pronotosh2 <127610730+pronotosh2@users.noreply.github.com>
2023-03-23 11:59:35 +0600

    Create Class06-2:00pm-19-03-2023-Sunday-City_IT_Park-Khalishpur.py

---
+++ Class06-2:00pm-19-03-2023-Sunday-City_IT_Park-Khalishpur.py
@@ -0,0 +1,151 @@
+                                            # Class06-2:00pm-19-03-2023-Sunday-City_IT_Park-Khalishpur.py   
+                                            # This file has a problem to run please run alternative         Code_only          
+                                            #we need to acquire soft skill
+
+# while loop:       With the while loop we can execute a set of statements as long as a condition is true.
+# It runs until interrupted 
+
+
+
+while True:                                                                                        
+    print(900)       
+                                                #900
+                                                #900
+                                                #.............................................................................................
+                                                #click on TERMINAL AND press ctrl + c . It will be interrupted(KeyboardInterrupt)
+
+
+
+                                                # sometimes Shift + F5       also work  
+
+
+                        #discussed :           skippable
+                        #google-1988     -Internet 1-HTML
+                        #                - web -1 -CSS
+                        #                - web -0.1/1.0 -js -jquery -pagination 
+                        #         -PHP(aka Personal Home Page) - web 2.0 
+                        #                                        reload, loading bar, react
+
+
+                        #in mobile facebook is a web view app because it redirected to an webpage/website.
+                        #                                                                                xlms,html               swift -IOS
+                        #         youtube also is a web view app.                       South korea  - VIVE STUDIOS is professional visualization company that offers all types of media in the technical background of VR, AR, CGI, VFX, and film. 
+                        #   google bird ,     dalle open AI -Microsoft
+                        #Ctrl + C for stoping Terminal                  
+
+
+
+
+a=20
+while True:
+    if a == 1000:
+        break
+    a=a+1
+    print(a)
+
+
+a=0
+while True:
+    if a==100000:
+        break
+    a=a+1
+    print("Saikat= ",a)
+
+
+                    #Problem:                                                                       #Social engineering:    Social engineering refers to all techniques aimed at talking a target into revealing specific information or performing a specific action ..
+                    #Gupi is an alien who has come to earth to do research. Gupi came with 5000 kg of food. Gupi consumes half of the food he brought in on the first day. Next day he will eat half of the stocked food. After the next day, he will eat half of the stockpiled food of that day. He will research on earth until his food stock drops to 1 kg or less. 
+                    # How long will he stay on earth?
+                    #
+
+
+
+                    #Home work solution:
+                    
+
+name=["akash","sadiya","mim","sumaiya","seheli"]
+country=["bangladesh","usa","india"]
+for i in country:
+    for j in name:
+        if i[0]==j[-1]:
+            print(f"{j} is King")
+        print("{j} is commoner")
+
+
+
+                                                                    #saikat218
+                                                                    #Nasifshezan
+                                                                    #Terminal --> exit()
+#Test
+
+for i in range(1,100):
+    print(i)                                            # 1 to 99
+
+
+
+for i in range(1,101):
+    print(i)                                            # 1 to 100
+
+
+a=5
+for i in range (1,10):
+    print(a,"X",i,"=",a*i)                                     #5's multiplication 
+
+
+
+
+
+
+
+#saikat218         #King.py
+
+a=['Burundi','Somalia','Mozambique','Madagascar','Sierra Leone','Afghanistan','Liberia','Niger']
+b=['Shuvo','Nayeem','Maruf','Pial','Opu','Maya','Leamon','Nixon','Mimi','Tony']
+for name_a in a:
+    for name_b in b:
+        if name_a[0].lower() == name_b[-1].lower():
+            print(name_b, "The King of ", name_a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Nasifshezan
+
+country= ["bangladesh","india","nepal","bhutan","srilanka","pakisthan"]
+friend= ["habib","ira","shezan","mehedi","sazid","omer"]
+
+for index,x in enumerate(country):
+    print(country[index][0].lower(),friend[index][-1].lopwer())
+    if (country[index][0].lower() == friend[index][0].lower() and country[index][-1].lower() == friend[index][-1].lower()):
+        print("I am not")
+    else:
+        print("I am the king")          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                         #For any question about this class , Our instructor:01990 709752 
+
+
+                         #        Thanks for your patience.
+
