34f6ca2d6900703880f4c8c4c6a3b70bf5b39d26
pronotosh2 <127610730+pronotosh2@users.noreply.github.com>
2023-04-09 22:15:20 +0600

    Create HW--2:00pm-09-04-2023-Sunday-City_IT_Park-Khalishpur.py

---
+++ HW--2:00pm-09-04-2023-Sunday-City_IT_Park-Khalishpur.py
@@ -0,0 +1,97 @@
+#HW--02:00pm-09-04-2023-Sunday-City_IT_Park-Khalishpur.py 
+
+
+
+                                        # Dictionary:
+                                        # Dictionaries are used to store data values in key:value pairs.
+                                        
+                                        # A dictionary is a collection which is ordered*, changeable and do not allow duplicates.
+                                        
+                                        # As of Python version 3.7, dictionaries are ordered. In Python 3.6 and earlier, dictionaries are unordered.
+                                        
+                                        # Dictionaries are written with curly brackets, and have keys and values:
+
+
+
+
+
+
+
+class Profile:
+    def __init__(self,name,roll,registration,_class,age,address,favorite_place,favorite_sport,email,password,phone_no,facebook,github,employers_contact):
+        self.name=name
+        self.roll=roll
+        self.registration=registration
+        self._class=_class
+        self.age=age
+        self.address=address
+        self.favorite_place=favorite_place
+        self.favorite_sport=favorite_sport
+        self.email=email
+        self.password=password
+        self.phone_no=phone_no
+        self.facebook=facebook
+        self.github=github
+        self.employers_contact=employers_contact
+
+    def public(self):
+        print("Name: " + self.name, ", Roll: " + self.roll, ", Class: " + self._class, ", Age: " + self.age, ", Phone no: " + self.phone_no, ", FB: " + self.facebook, ", GitHub: " + self.github)
+    def friend(self):
+        print(" Registration: " + self.registration,", Address: " + self.address, ", Favorite place: " + self.favorite_place, ", Favorite Sport: " + self.favorite_sport, ", Email: " + self.email)
+    def only_me(self):
+        print(" Password: " + self.password,", Employer's contact: " + self.employers_contact)
+
+Saikat=Profile("Saikat","001","221","5","22","New_market","Sundarbans","Cricket","saikat@gmail.com","sfdfdsfj","01777888999","Saikat12","github/saikat5","01666777888")
+Akash=Profile("Akash","002","222","1","21","Sonadanga","Sundarbans","Cricket","Akash@gmail.com","sfAkshfj","01777008999","Akasht12","github/Akash5","01666777888")
+Nabab=Profile("Nabab","003","223","3","22","Khalishpur","Sylhet","Cricket","Nabab@gmail.com","sfNababj","01777338999","Nabab12","github/Nabab5","01600743888")
+Milon=Profile("Milon","004","224","5","23","Boyra","Sundarbans","Football","Milon@gmail.com","sEkmmrn","01777855999","Milon12","github/Milon5","01666547888")
+Emran=Profile("Emran","005","225","3","25","Zero_point","Kuakata","Badminton","Emran@gmail.com","sfdfdsfj","01733388999","Emran12","github/Emran5","01666677888")
+Nayon=Profile("Nayon","006","226","2","24","New_market","Sundarbans","Football","Nayon@gmail.com","sfdsfj","01756588999","Nayon12","github/Nayon5","01666887888")
+Kamal=Profile("Kamal","007","227","1","23","Rupsa","Dhaka","Cricket","Kamal@gmail.com","sfdfdsfj","01775888999","Kamal12","github/Kamal5","01666877888")
+Rafiq=Profile("Rafiq","008","228","5","22","New_market","Sundarbans","Cricket","Rafiq@gmail.com","sfdfdsfj","01775688999","Rafiq","github/Rafiq5","01666787888")
+
+Saikat.public()
+Saikat.friend()
+Saikat.only_me()
+
+print("________________________________________________________")
+
+Akash.public()
+Akash.friend()
+Akash.only_me()
+
+print("________________________________________________________")
+
+Nabab.public()
+Nabab.friend()
+Nabab.only_me()
+
+print("________________________________________________________")
+
+Milon.public()
+Milon.friend()
+Milon.only_me()
+
+print("________________________________________________________")
+
+Emran.public()
+Emran.friend()
+Emran.only_me()
+
+print("________________________________________________________")
+
+Nayon.public()
+Nayon.friend()
+Nayon.only_me()
+
+print("________________________________________________________")
+
+Kamal.public()
+Kamal.friend()
+Kamal.only_me()
+
+print("________________________________________________________")
+
+Rafiq.public()
+Rafiq.friend()
+Rafiq.only_me()
